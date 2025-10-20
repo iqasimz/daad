c247a9ba1db49f8a9fbe04e36a099b50abedf87e
Muhammad Qasim Hussain <86079520+iqasimz@users.noreply.github.com>
2025-10-21 01:33:43 +0500

    Fix formatting issue in vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        index:    resolve(__dirname, 'index.html'),
+        catalogue:resolve(__dirname, 'catalogue.html'),
+        scholar:  resolve(__dirname, 'scholar.html'),
+        book:     resolve(__dirname, 'book.html'),
+        contact:  resolve(__dirname, 'contact.html'),
+      }
+    }
+  }
+});
